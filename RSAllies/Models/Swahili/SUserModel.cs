6919e4c740247f6b49dc56f750773f9bfe361556
hei karata <herishine00@gmail.com>
2024-05-18 14:44:43 +0300

    `Added new user models and updated Register view`
    Implemented significant changes to the user registration process.
    
    In `Register.cshtml`, fixed a syntax error by closing the ViewData["Title"] line properly and added a model directive. Introduced a new div structure with "Container" and "Inner" divs, and a form with a validation summary for the model was added inside the "Inner" div. An empty div with class "tab" was also added inside the form.
    
    Two new classes, `UserModel` and `SUserModel`, were added to the `RSAllies.Models.English` namespace in `UserModel.cs` and `SUserModel.cs` respectively. Both classes contain properties for user details.
    
    In `Register.cshtml.css`, added CSS styles for the new "Container" and "Inner" divs to enhance the user interface.
    
    Lastly, in `_ViewImports.cshtml`, imported the RSAllies and RSAllies.Models.English namespaces and added the Microsoft.AspNetCore.Mvc.TagHelpers tag helper.

---
+++ RSAllies/Models/Swahili/SUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RSAllies.Models.Swahili
+{
+    public class SUserModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Identification { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+        public bool IsForeigner { get; set; }
+        public Guid GenderId { get; set; }
+        public Guid EducationLevelId { get; set; }
+        public Guid LicenseClassId { get; set; }
+    }
+}
