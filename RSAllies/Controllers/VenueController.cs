af0ed5a66ba5c0947fa9d08a3fa3a09cc7010569
hei karata <herishine00@gmail.com>
2024-05-21 22:36:09 +0300

    Added venue management feature and updated Dashboard UI
    A new feature for managing venues has been implemented. This includes the addition of a new `venues` folder in `RSAllies.csproj`, a `VenueController` with `Index` and `Add` action methods, and corresponding views and styles for these methods in `Add.cshtml`, `Add.cshtml.css`, `Index.cshtml`, and `Index.cshtml.css`. The `Venue` class has also been added with appropriate properties and validation.
    
    Significant updates have been made to the `Dashboard.cshtml` and `Dashboard.cshtml.css` files. These changes include the addition of new divs, updates to existing divs, the inclusion of a new `Scripts` section, and various style changes. The `Container` class, `highcharts-figure` and `highcharts-data-table table` classes, and several ids have been updated or added with new styles. Media queries for `thirdContainer` have also been added for screens with a max-width of 768px.

---
+++ RSAllies/Controllers/VenueController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RSAllies.Controllers
+{
+    public class VenueController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ RSAllies/Models/Venue.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RSAllies.Models
+{
+    public class Venue
+    {
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        [Required]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        [Range(50, 2000, MinimumIsExclusive = true, MaximumIsExclusive = true, ErrorMessage = "The capacity should be from 50 to 2000")]
+        [RegularExpression(@"^\d{2,4}$", ErrorMessage = "The capacity should be a number")]
+        public int Capacity { get; set; }
+
+        [Required]
+        public Guid RegionId { get; set; }
+
+        [Required]
+        public Guid DistrictId { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
