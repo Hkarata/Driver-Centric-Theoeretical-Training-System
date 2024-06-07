7aee7765134f2bc116324f26c11e67f50a9f4e07
hei karata <herishine00@gmail.com>
2024-06-08 00:41:54 +0300

    `Added SessionsController, CreateSession model, and related views`
    ` `
    
    `Added a new SessionsController with Index and Create actions in the RSAllies.Controllers namespace. Created a new model class, CreateSession, in the RSAllies.Models namespace with properties for VenueId, Date, StartTime, and EndTime. Added a new Razor view, Create.cshtml, which uses the CreateSession model and includes a form for creating a new session. The form includes fields for Date, StartTime, and EndTime, with the EndTime field being read-only and automatically set to 45 minutes after the StartTime. Also added a new CSS file, Create.cshtml.css, for styling the Create.cshtml view. Updated the Index.cshtml view to set the ViewData["Title"] to "Sessions".`

---
+++ RSAllies/Controllers/SessionsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RSAllies.Controllers
+{
+    public class SessionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ RSAllies/Models/CreateSession.cs
@@ -0,0 +1,10 @@
+﻿namespace RSAllies.Models
+{
+    public class CreateSession
+    {
+        public Guid VenueId { get; set; }
+        public DateTime Date { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+    }
+}
