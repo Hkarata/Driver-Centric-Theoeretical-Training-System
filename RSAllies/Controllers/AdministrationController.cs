e99b9622262b7709d803814d7d29fc480caeeaf3
hei karata <herishine00@gmail.com>
2024-06-28 21:52:25 +0300

    Enhance AdminController with async and analytics
    - Introduced Newtonsoft.Json and RSAllies.Analytics namespaces to AdministrationController for JSON processing and analytics.
    - Modified the AdministrationController constructor to include an ApiService parameter, enabling API data fetching.
    - Transitioned the Index action method to async, improving scalability and responsiveness.
    - Added new async methods for data seeding (gender, age groups, education levels, peak booking times), enhancing data processing and maintainability.
    - Updated Index.cshtml with dynamic Highcharts data integration and improved pie chart animation logic.
    - Adjusted CSS and JavaScript in Index.cshtml to align with the updated element ID and to log AgeGroupCounts for debugging.

--- RSAllies/Controllers/AdministrationController.cs
+++ RSAllies/Controllers/AdministrationController.cs
@@ -1,14 +1,191 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RSAllies.Analytics;
 using RSAllies.Models;
 using RSAllies.Services;
 
 namespace RSAllies.Controllers
 {
-    public class AdministrationController(ApiClient apiClient) : Controller
+    public class AdministrationController(ApiClient apiClient, ApiService apiService) : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            await SeedGenderCount();
+            await SeedAgeGroupCounts();
+            await SeedEducationLevelData();
+            await SeedPeekBookingDays();
             return View();
+        }
+
+        public async Task SeedGenderCount()
+        {
+            var result = await apiService.GetGenderCount();
+
+            int maleCount = 0;
+            int femaleCount = 0;
+
+            if (result.IsSuccess)
+            {
+                var dataset = result.Value;
+
+                foreach (var item in dataset)
+                {
+                    if (item.GenderType == "Male")
+                    {
+                        maleCount = item.Count;
+                    }
+                    else
+                    {
+                        femaleCount = item.Count;
+                    }
+
+                }
+            }
+
+            ViewBag.MaleCount = maleCount;
+            ViewBag.FemaleCount = femaleCount;
+        }
+
+        public async Task SeedAgeGroupCounts()
+        {
+            var result = await apiService.GetAgeGroups();
+
+            int[] ageGroupCounts = new int[] { 0, 0, 0, 0, 0, 0 };
+
+            if (result.IsSuccess)
+            {
+                var dataset = result.Value;
+
+                foreach (var item in dataset)
+                {
+                    switch (item.AgeGroup)
+                    {
+                        case "18-25":
+                            ageGroupCounts[0] = item.Count;
+                            break;
+                        case "26-33":
+                            ageGroupCounts[1] = item.Count;
+                            break;
+                        case "34-41":
+                            ageGroupCounts[2] = item.Count;
+                            break;
+                        case "42-49":
+                            ageGroupCounts[3] = item.Count;
+                            break;
+                        case "50-55":
+                            ageGroupCounts[4] = item.Count;
+                            break;
+                        default:
+                            ageGroupCounts[5] = item.Count;
+                            break;
+                    }
+                }
+            }
+
+            ViewBag.AgeGroupCounts = ageGroupCounts;
+        }
+
+        public async Task SeedEducationLevelData()
+        {
+            var result = await apiService.GetEducationLevelCount();
+
+            int[] educationLevelCounts = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+            int[] degreeCounts = new int[] { 0, 0, 0 };
+
+            if(result.IsSuccess)
+            {
+                var dataset = result.Value;
+
+                foreach (var item in dataset)
+                {
+                    switch (item.Level)
+                    {
+                        case "Uneducated":
+                            educationLevelCounts[0] = item.Count;
+                            break;
+                        case "Class 7":
+                            educationLevelCounts[1] = item.Count;
+                            break;
+                        case "Form 2":
+                            educationLevelCounts[2] = item.Count;
+                            break;
+                        case "Form 4":
+                            educationLevelCounts[3] = item.Count;
+                            break;
+                        case "Form 6":
+                            educationLevelCounts[4] = item.Count;
+                            break;
+                        case "Diploma":
+                            educationLevelCounts[5] = item.Count;
+                            break;
+                        default:
+                            educationLevelCounts[6] += item.Count;
+                            break;
+                    }
+                }
+
+                foreach (var item in dataset)
+                {
+                    switch (item.Level)
+                    {
+                        case "Bachelor's Degree":
+                            degreeCounts[0] = item.Count;
+                            break;
+                        case "Master's Degree":
+                            degreeCounts[1] = item.Count;
+                            break;
+                        default:
+                            degreeCounts[2] = item.Count;
+                            break;
+                    }
+                }
+            }
+
+            ViewBag.EducationLevelCounts = educationLevelCounts;
+            ViewBag.DegreeCounts = degreeCounts;
+        }
+
+        public async Task SeedPeekBookingDays()
+        {
+            var result = await apiService.GetPeekBookingDays();
+
+            int[] bookingCounts = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+
+            if (result.IsSuccess)
+            {
+                var dataset = result.Value;
+
+                foreach (var item in dataset)
+                {
+                    switch (item.Day.ToLower())
+                    {
+                        case "monday":
+                            bookingCounts[0] = item.Bookings;
+                            break;
+                        case "tuesday":
+                            bookingCounts[1] = item.Bookings;
+                            break;
+                        case "wednesday":
+                            bookingCounts[2] = item.Bookings;
+                            break;
+                        case "thursday":
+                            bookingCounts[3] = item.Bookings;
+                            break;
+                        case "friday":
+                            bookingCounts[4] = item.Bookings;
+                            break;
+                        case "saturday":
+                            bookingCounts[5] = item.Bookings;
+                            break;
+                        default:
+                            bookingCounts[6] = item.Bookings;
+                            break;
+                    }
+                }
+            }
+
+            ViewBag.BookingCounts = JsonConvert.SerializeObject(bookingCounts);
+
         }
 
         [ActionName("Create-Admin")]
